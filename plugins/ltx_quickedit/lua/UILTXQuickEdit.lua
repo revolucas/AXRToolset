27b3c3ded0a86f279848bc1591f47503aa28e064
revolucas <revolucas@gmail.com>
2016-09-12 19:18:19 -0400

    = moved Modify GUI into single script

--- plugins/ltx_quickedit/lua/UILTXQuickEdit.lua
+++ plugins/ltx_quickedit/lua/UILTXQuickEdit.lua
@@ -76,7 +76,7 @@ function cUILTXQuickEdit:OnScriptControlAction(hwnd,event,info) -- needed becaus
 			--Msg("event=%s LVGetNext=%s txt=%s",event,LVGetNext(self.ID,"0","UILTXQuickEditLV"..tab),txt)
 			if (txt and txt ~= "" and not self.listItemSelected) then 
 				self.listItemSelected = txt
-				UILTXQuickEditModify.GetAndShow()
+				GetAndShowModify()
 			end
 		end
 	elseif (hwnd == GuiControlGet(self.ID,"hwnd","UILTXQuickEditSection"..tab)) then 	
@@ -199,4 +199,110 @@ function cUILTXQuickEdit:FillListView(tab)
 	for i=1,#self.fields do
 		LV("LV_ModifyCol",self.ID,tostring(i+1),"AutoHdr")
 	end
+end
+---------------------------------
+cUILTXQuickEditModify = Class{__includes={cUIBase}}
+function cUILTXQuickEditModify:init(id)
+	cUIBase.init(self,id)
+end
+
+function cUILTXQuickEditModify:Show(bool)
+	cUIBase.Show(self,bool)
+end 
+
+function cUILTXQuickEditModify:Create()
+	cUIBase.Create(self)
+end
+
+function cUILTXQuickEditModify:Reinit()
+	cUIBase.Reinit(self)
+	
+	self:Gui("+AlwaysonTop")
+	self:Gui("Font|s10|Verdana")
+	
+	local wnd = UILTXQuickEdit.Get()
+	if (wnd.listItemSelected == nil) then 
+		return Msgbox("An error has occured. listItemSelected = nil!")
+	end
+	
+	local list = wnd.list[wnd.listItemSelected]
+	
+	if not (list) then 
+		return Msgbox("An error has occured. list = nil!")
+	end
+	
+	local fname = list.fname
+	
+	self:Gui("Add|Text|w300 h30|%s",fname)
+	
+	local tab = ahkGetVar("UILTXQuickEditTab")
+
+	local y = 35
+	for field,v in pairs(list) do 
+		if (field ~= "fname") then
+			self:Gui("Add|Text|x5 y%s w300 h30|%s",y,field)
+			self:Gui("Add|Edit|x200 y%s w800 h30 vUIModifyEdit%s|%s",y,field,v)
+			y = y + 30
+		end
+	end
+ 
+	self:Gui("Add|Button|gOnScriptControlAction x12 default vUIModifyAccept|Accept")
+	self:Gui("Add|Button|gOnScriptControlAction x+4 vUIModifyCancel|Cancel")
+	self:Gui("Show|center|Edit Values")
+	self:Gui("Default")
+end
+
+function cUILTXQuickEditModify:OnGuiClose(idx) -- needed because it's registered to callback
+	cUIBase.OnGuiClose(self,idx)
+end 
+
+function cUILTXQuickEditModify:Destroy()
+	cUIBase.Destroy(self)
+	
+	UILTXQuickEdit.Get().listItemSelected = nil 
+end
+
+function cUILTXQuickEditModify:OnScriptControlAction(hwnd,event,info) -- needed because it's registered to callback
+
+	self:Gui("Submit|NoHide")
+	local tab = ahkGetVar("UILTXQuickEditTab") or "1"
+		
+	if (hwnd == GuiControlGet(self.ID,"hwnd","UIModifyAccept")) then
+		local wnd = UILTXQuickEdit.Get()
+		local list = assert(wnd.list[wnd.listItemSelected])
+		local fname = list.fname
+	
+		assert(wnd.ltx[fname])
+		
+		for field,v in pairs(list) do 
+			if (field ~= "fname") then
+				local val = ahkGetVar("UIModifyEdit"..field)
+				wnd.ltx[fname]:SetValue(wnd.listItemSelected,field,val)
+			end
+		end
+
+		wnd.ltx[fname]:SaveExt()
+		
+		self:Show(false)
+		
+		wnd:FillListView(tab)
+	elseif (hwnd == GuiControlGet(self.ID,"hwnd","UIModifyCancel")) then
+		self:Show(false)
+	end
+end
+
+function cUILTXQuickEditModify:Gui(...)
+	cUIBase.Gui(self,...)
+end
+------------------------
+local oUILTXQuickEditModify = nil
+function GetModify()
+	if not (oUILTXQuickEditModify) then 
+		oUILTXQuickEditModify = cUILTXQuickEditModify("2")
+	end
+	return oUILTXQuickEditModify
+end
+
+function GetAndShowModify()
+	GetModify():Show(true)
 end
--- plugins/trade_editor/lua/UITraderEditor.lua
+++ plugins/trade_editor/lua/UITraderEditor.lua
@@ -73,7 +73,7 @@ function cUITraderEditor:OnScriptControlAction(hwnd,event,info) -- needed becaus
 			local txt = LVGetText(self.ID,LVGetNext(self.ID,"0","UITraderEditorLV"..tab),"1")
 			if (txt and txt ~= "" and not self.listItemSelected) then
 				self.listItemSelected = txt
-				UIModify.GetAndShow()
+				GetAndShowModify()
 			end
 		end
 	elseif (hwnd == GuiControlGet(self.ID,"hwnd","UITraderEditorSection"..tab)) then 	
@@ -180,4 +180,140 @@ function cUITraderEditor:FillListView(typ)
 	for i=1,16 do
 		LV("LV_ModifyCol",self.ID,tostring(i),"AutoHdr")
 	end
+end
+------------------------------------------
+cUIModify = Class{__includes={cUIBase}}
+function cUIModify:init(id)
+	cUIBase.init(self,id)
+end
+
+function cUIModify:Show(bool)
+	cUIBase.Show(self,bool)
+end 
+
+function cUIModify:Create()
+	cUIBase.Create(self)
+end
+
+function cUIModify:Reinit()
+	cUIBase.Reinit(self)
+	
+	self:Gui("+AlwaysonTop")
+	self:Gui("Font|s10|Verdana")
+	
+	local wnd = UITraderEditor.Get()
+	local list = wnd.list[wnd.listItemSelected]
+	if not (list) then 
+		return 
+	end
+	local fname = trim_directory(list.path)
+	
+	self:Gui("Add|Text|w300 h30|%s",fname)
+	
+	local tab = ahkGetVar("UITraderEditorTab")
+	if (tab == "1") then
+		self:Gui("Add|Edit|w300 h30 vUIModifyEdit1|%s",list.buy_condition)
+		self:Gui("Add|Edit|w300 h30 vUIModifyEdit2|%s",list.sell_condition)
+		
+		local cnt = 3
+		for sec,v in pairs(list.buy_supplies) do 
+			self:Gui("Add|Edit|w300 h30 vUIModifyEdit%s|%s",cnt,v)
+			cnt = cnt + 1
+		end
+	else 
+		local t
+		if (fname == "death_generic.ltx") then 
+			t = {"keep_items","item_count"}
+		else 
+			t = {"base","stalker","bandit","killer","dolg","freedom","army","monolith","csky","ecolog"}
+		end
+		
+		for i=1,#t do 
+			self:Gui("Add|Edit|w300 h30 vUIModifyEdit%s|%s",i,list[t[i]])
+		end
+	end
+ 
+	self:Gui("Add|Button|gOnScriptControlAction x12 default vUIModifyAccept|Accept")
+	self:Gui("Add|Button|gOnScriptControlAction x+4 vUIModifyCancel|Cancel")
+	self:Gui("Show|center|Edit Values")
+	self:Gui("Default")
+end
+
+function cUIModify:OnGuiClose(idx) -- needed because it's registered to callback
+	cUIBase.OnGuiClose(self,idx)
+end 
+
+function cUIModify:Destroy()
+	cUIBase.Destroy(self)
+	
+	UITraderEditor.Get().listItemSelected = nil 
+end
+
+function cUIModify:OnScriptControlAction(hwnd,event,info) -- needed because it's registered to callback
+	if (hwnd == GuiControlGet(self.ID,"hwnd","UIModifyAccept")) then
+		self:Gui("Submit|NoHide")
+		local tab = ahkGetVar("UITraderEditorTab")
+		
+		local wnd = UITraderEditor.Get()
+		local list = assert(wnd.list[wnd.listItemSelected])
+		local fname = trim_directory(list.path)
+	
+		assert(wnd.ltx[fname])
+
+		if (tab == "1") then
+			local t = {wnd.ltx[fname]:GetValue("trader","buy_condition"),wnd.ltx[fname]:GetValue("trader","sell_condition")}
+			for i=1,#t do
+				local v = ahkGetVar("UIModifyEdit"..i) or ""
+				wnd.ltx[fname]:SetValue(t[i],list.section,v)
+			end
+			local cnt = 3
+			for sec,t in pairs(list.buy_supplies) do 
+				local v = ahkGetVar("UIModifyEdit"..cnt)
+				wnd.ltx[fname]:SetValue(sec,list.section,v)
+				cnt = cnt + 1
+			end
+		else 
+			if (fname == "death_generic.ltx") then
+				local t = {"keep_items","item_count"}
+				for i=1,#t do
+					local v = ahkGetVar("UIModifyEdit"..i)
+					if (v and v ~= "") then
+						wnd.ltx[fname]:SetValue(t[i],list.section,v)
+					end
+				end
+			elseif (fname == "death_items_by_communities.ltx") then 
+				local t = {"base","stalker","bandit","killer","dolg","freedom","army","monolith","csky","ecolog"}
+				for i=1,#t do
+					local v = ahkGetVar("UIModifyEdit"..i)
+					if (v and v ~= "") then
+						wnd.ltx[fname]:SetValue(t[i],list.section,v)
+					end
+				end
+			end
+		end
+		
+		wnd.ltx[fname]:SaveExt()
+		
+		self:Show(false)
+		
+		wnd:FillListView(tab)
+	elseif (hwnd == GuiControlGet(self.ID,"hwnd","UIModifyCancel")) then
+		self:Show(false)
+	end
+end
+
+function cUIModify:Gui(...)
+	cUIBase.Gui(self,...)
+end
+-------------------------------------------------
+local oUIModify = nil
+function GetModify()
+	if not (oUIModify) then 
+		oUIModify = cUIModify("2")
+	end
+	return oUIModify
+end
+
+function GetAndShowModify()
+	GetModify():Show(true)
 end
